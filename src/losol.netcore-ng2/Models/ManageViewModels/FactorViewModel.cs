9a0c3b854d329e2bea39e5edfa22cf2a097efba2
losol <losvik@gmail.com>
2016-05-21 23:59:18 +0200

    Add project files.

---
+++ src/losol.netcore-ng2/Models/ManageViewModels/FactorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace losol.netcore_ng2.Models.ManageViewModels
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
---
+++ src/losol.netcore-ng2/Services/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace losol.netcore_ng2.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}

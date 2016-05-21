9e8a496c29e6032f51b134ef3bf9a3df53fb95e5
losol <losvik@gmail.com>
2016-05-22 01:26:21 +0200

    init

--- src/losol.netcore-ng2/gulpfile.js
+++ src/losol.netcore-ng2/gulpfile.js
@@ -1,11 +1,14 @@
-﻿/// <binding Clean='clean' />
+﻿/// <binding AfterBuild='ts' Clean='clean' />
 "use strict";
 
 var gulp = require("gulp"),
     rimraf = require("rimraf"),
     concat = require("gulp-concat"),
     cssmin = require("gulp-cssmin"),
-    uglify = require("gulp-uglify");
+    uglify = require("gulp-uglify"),
+    ts = require('gulp-typescript'),
+    gulp = require('gulp'),
+    clean = require('gulp-clean');
 
 var webroot = "./wwwroot/";
 
@@ -15,7 +18,8 @@ var paths = {
     css: webroot + "css/**/*.css",
     minCss: webroot + "css/**/*.min.css",
     concatJsDest: webroot + "js/site.min.js",
-    concatCssDest: webroot + "css/site.min.css"
+    concatCssDest: webroot + "css/site.min.css",
+    lib: webroot + "lib/"
 };
 
 gulp.task("clean:js", function (cb) {
@@ -43,3 +47,35 @@ gulp.task("min:css", function () {
 });
 
 gulp.task("min", ["min:js", "min:css"]);
+
+//Copys scripts to wwwroot/lib folder
+// Thanks  MITHUN PATTANKAR, http://www.mithunvp.com/angular-2-in-asp-net-5-typescript-visual-studio-2015/
+gulp.task("CopyScripts", () => {
+    gulp.src([
+            'core-js/client/shim.min.js',
+            'es6-shim/es6-shim.min.js',
+            'systemjs/dist/system-polyfills.js',
+            'systemjs/dist/system.src.js',
+            'reflect-metadata/Reflect.js',
+            'rxjs/**',
+            'zone.js/dist/**',
+            '@angular/**',
+            'jquery/dist/jquery.*js',
+            'bootstrap/dist/js/bootstrap*.js'
+    ], {
+        cwd: "node_modules/**"
+    })
+        .pipe(gulp.dest("./wwwroot/lib"));
+});
+
+
+// Compile typescript
+var tsProject = ts.createProject('scripts/tsconfig.json');
+gulp.task('ts', function (done) {
+    //var tsResult = tsProject.src()
+    var tsResult = gulp.src([
+            "scripts/*.ts"
+    ])
+        .pipe(ts(tsProject), undefined, ts.reporter.fullReporter());
+    return tsResult.js.pipe(gulp.dest('./wwwroot/app'));
+});
